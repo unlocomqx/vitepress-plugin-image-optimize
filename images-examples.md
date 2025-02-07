4a3ae5eeef2d2ed94a5dda7a41abe494a4a1c6aa
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-02-07 17:05:14 +0100

    convert images

--- images-examples.md
+++ images-examples.md
@@ -4,7 +4,7 @@ This page contains examples of images in VitePress.
 
 ![Nature](/nature-0007.jpg)
 
-![Nature](/nature-0008.jpg)
+![Nature](/nature-0008@2x.jpg)
 
 A relative path
 
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import  {PluginWithOptions, Token} from 'markdown-it'
-import  type MarkdownIt from 'markdown-it'
+import type MarkdownIt from 'markdown-it'
+import {PluginWithOptions} from 'markdown-it'
 import * as path from 'node:path'
 import * as fs from "node:fs"
-import { imageSize } from 'image-size'
+import sharp from 'sharp'
+import {imageSize} from 'image-size'
 
 export const optimizeImages: () => PluginWithOptions = () => {
   return (md: MarkdownIt) => {
     const imageRule = md.renderer.rules.image!
 
-    md.renderer.rules.image = (tokens: Token[], idx, options, env, self) => {
+    md.renderer.rules.image = (tokens, idx, options, env, self) => {
       const token = tokens[idx]
       let img_alt = md.utils.escapeHtml(token.content)
-      let img_src = md.utils.escapeHtml(token.attrGet('src'))
-      let img_title = md.utils.escapeHtml(token.attrGet('title'))
+      let img_src = md.utils.escapeHtml(token.attrGet('src') || '')
+      let img_title = md.utils.escapeHtml(token.attrGet('title') || '')
       let local = !/^https?:\/\//.test(img_src)
       if (!local) {
         return imageRule(tokens, idx, options, env, self)
@@ -21,20 +22,67 @@ export const optimizeImages: () => PluginWithOptions = () => {
       const public_path = path.resolve('public')
       let img_path = path.join(public_path, img_src)
       if (img_src.startsWith('.')) {
-        console.log(path.dirname(env.path))
         img_path = path.join(path.dirname(env.path), img_src)
       }
 
-      if(!fs.existsSync(img_path)) {
+      if (!fs.existsSync(img_path)) {
         console.warn(`VitePress Image Optimize: Image not found: ${img_src}`)
         return imageRule(tokens, idx, options, env, self)
       }
 
       const {width, height, type} = imageSize(img_path)
+      let scale = 1
+      if (img_src.includes('@2x')) {
+        scale = 2
+      }
 
-      console.log(size)
+      if (!width || !height || !type) {
+        console.warn(`VitePress Image Optimize: Image size could not be determined: ${img_src}`)
+        return imageRule(tokens, idx, options, env, self)
+      }
 
-      return imageRule(tokens, idx, options, env, self)
+      const dist = path.resolve('public/webp')
+      fs.mkdirSync(dist, {recursive: true})
+
+      const mtime = fs.statSync(img_path).mtime
+      const dist_webp = `${path.join(dist, img_src)}-${mtime.getTime()}.webp`
+
+      if (!fs.existsSync(dist_webp)) {
+        sharp(img_path)
+          .webp({quality: 80})
+          .toFile(dist_webp)
+      }
+
+      const dist_webp_1x = dist_webp.replace('@2x', '')
+      if (scale === 2) {
+        if (!fs.existsSync(dist_webp_1x)) {
+          sharp(img_path)
+            .resize({
+              width: width / 2,
+            })
+            .webp({quality: 80})
+            .toFile(dist_webp_1x)
+        }
+      }
+
+      let style_width = `${width}px;`
+      if (scale === 2) {
+        style_width = `${width / 2}px;`
+      }
+
+      const img_tag = `
+        <img 
+          src="${scale === 1 ? dist_webp.replace(public_path, '') : dist_webp_1x.replace(public_path, '')}" 
+          ${scale === 2 ? `srcset="${dist_webp_1x.replace(public_path, '')} 1x, ${dist_webp.replace(public_path, '')} 2x"` : ''}
+          alt="${img_alt}" 
+          title="${img_title}" 
+          width="${width}" 
+          height="${height}" 
+          style="width: ${style_width}" 
+        />
+      `
+
+      return img_tag
     }
   }
 }
